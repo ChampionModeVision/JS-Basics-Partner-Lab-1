27495fabbaeb57260433f632acc5c21c6ffb9399
ChampionModeVision <hwjnthn@live.com>
2020-11-17 18:56:54 -0500

    we almost there

---
+++ opp.js
@@ -0,0 +1,22 @@
+
+
+
+let first = Number(prompt("Type First Number"));
+
+let second = Number(prompt("Type Second Number"));
+
+//if(number%5 === 0 && number%3 === 0 ){
+    console.log(first/second);
+//}
+
+
+//else if(number%3 === 0){
+  //  console.log("Fizz")
+
+//}
+
+//else if(number%5 === 0){
+   // console.log("Buzz")
+
+
+//}
